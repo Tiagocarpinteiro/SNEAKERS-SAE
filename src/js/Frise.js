0572b811358354a6e9762011e75cafd41a2c73ee
Thibaut <thibautdebroucke@gmail.com>
2023-12-19 10:00:06 +0100

    pas de scroll encore

--- src/js/Frise.js
+++ src/js/Frise.js
@@ -4,10 +4,12 @@ gsap.registerPlugin(ScrollTrigger)
 
 document.getElementById("slides").onscroll = () => {
     document.getElementById("frise").scroll(0, ((document.getElementById("slides").scrollTop*document.getElementById("frise").scrollHeight)/document.getElementById("slides").scrollHeight)*0.75)
-    console.log(document.getElementById("frise").scrollTop, document.getElementById("frise").scrollHeight)
 }
 
-gsap.to("#title-2", {
-    scrollTrigger: "#origines",
-    fontSize: "32px"
-})
+if(document.getElementById("slides").scrollTop >= document.getElementById("slides").scrollHeight/9) {
+    console.log(document.getElementById("slides").scrollHeight/9)
+    gsap.to("#title-2", {
+        scrollTrigger: "#origines",
+        fontSize: "32px"
+    })
+}
