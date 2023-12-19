1475fa17985ccf16adfdc906524eb9cff52a7f96
Thibaut <thibautdebroucke@gmail.com>
2023-12-19 11:33:03 +0100

    1916

--- src/js/Frise.js
+++ src/js/Frise.js
@@ -1,15 +1,29 @@
 import {gsap} from "gsap";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
+import ScrollReveal from "scrollreveal";
 gsap.registerPlugin(ScrollTrigger)
+import LocomotiveScroll from "locomotive-scroll";
+
 
 document.getElementById("slides").onscroll = () => {
     document.getElementById("frise").scroll(0, ((document.getElementById("slides").scrollTop*document.getElementById("frise").scrollHeight)/document.getElementById("slides").scrollHeight)*0.75)
+    if(document.getElementById("slides").scrollTop >= document.getElementById("slides").scrollHeight/9) {
+        console.log(document.getElementById("slides").scrollHeight/9)
+        gsap.to("#title-2", {
+            scrollTrigger: "#origines",
+            fontSize: "32px"
+        })
+
+        gsap.to("#line-2", {
+            scrollTrigger: "#origines",
+            width: "20px"
+        })
+    }
 }
 
-if(document.getElementById("slides").scrollTop >= document.getElementById("slides").scrollHeight/9) {
-    console.log(document.getElementById("slides").scrollHeight/9)
-    gsap.to("#title-2", {
-        scrollTrigger: "#origines",
-        fontSize: "32px"
-    })
-}
+
+gsap.from(".scroll", {
+    scrollTrigger: "#origines",
+    x: 300,
+    delay: 2
+})
