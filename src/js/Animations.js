159d3d64ed4aa88d8f38e7f0f18edc6dfcb58e13
Thibaut <thibautdebroucke@gmail.com>
2023-12-20 12:43:48 +0100

    video

--- src/js/Animations.js
+++ src/js/Animations.js
@@ -305,3 +305,70 @@ gsap.to("#title-jordan", {
         toggleActions: "play none none reset"
     }
 });
+
+const video = document.querySelector(".video-background");
+let src = video.currentSrc || video.src;
+console.log(video, src);
+
+/* Make sure the video is 'activated' on iOS */
+function once(el, event, fn, opts) {
+    var onceFn = function (e) {
+        el.removeEventListener(event, onceFn);
+        fn.apply(this, arguments);
+    };
+    el.addEventListener(event, onceFn, opts);
+    return onceFn;
+}
+
+once(document.documentElement, "touchstart", function (e) {
+    video.play();
+    video.pause();
+});
+
+/* ---------------------------------- */
+/* Scroll Control! */
+
+gsap.registerPlugin(ScrollTrigger);
+
+let tl = gsap.timeline({
+    defaults: { duration: 1 },
+    scrollTrigger: {
+        start: "top top",
+        end: "bottom bottom",
+        scrub: true
+    }
+});
+
+once(video, "loadedmetadata", () => {
+    tl.fromTo(
+        video,
+        {
+            currentTime: 1
+        },
+        {
+            currentTime: video.duration || 38
+        }
+    );
+});
+
+/* When first coded, the Blobbing was important to ensure the browser wasn't dropping previously played segments, but it doesn't seem to be a problem now. Possibly based on memory availability? */
+setTimeout(function () {
+    if (window["fetch"]) {
+        fetch(src)
+            .then((response) => response.blob())
+            .then((response) => {
+                var blobURL = URL.createObjectURL(response);
+
+                var t = video.currentTime;
+                once(document.documentElement, "touchstart", function (e) {
+                    video.play();
+                    video.pause();
+                });
+
+                video.setAttribute("src", blobURL);
+                video.currentTime = t + 0.01;
+            });
+    }
+}, 1000);
+
+/* ---------------------------------- */
